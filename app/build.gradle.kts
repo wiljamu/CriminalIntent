49bcaa4dcc3a99e59300008494a0e9af1e2c04bb
hwilscam <hw55s@missouristate.edu>
2025-03-26 11:55:55 -0500

    Initial Commit - Ch. 9 App completed

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.hwilscam.criminalintent"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.hwilscam.criminalintent"
+        minSdk = 34
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.ui.test.android)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+
+}
